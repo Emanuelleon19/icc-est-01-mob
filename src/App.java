38b7eefda99cb387c62f658a7c24d96b88b93ffc
unknown <snsans001@gmail.com>
2024-09-25 12:56:29 -0700

    Ejercicios terminados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Metodos metodosOrdenamiento = new Metodos();
+     
+
+        int[] arreglo = {5,7,30,12,9};
+
+        int[] arregloOrdenado = metodosOrdenamiento.sortByBubble(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenado);
+
+        System.out.println("");
+        int[] arregloOrdenadoDes = metodosOrdenamiento.sortByBubbleDes(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenadoDes);
+
+        //ORDENAR LAS PALABRAS ALFABETICAMENTE DE LA CADENA DE TEXTO
+        System.out.println("");
+        String cadena = "Este es un ejemplo de texto para ordenar";
+        String[] palabras = cadena.split(" ");
+        String[] palabrasOrdenadas = metodosOrdenamiento.sortByBubblePalabras(palabras);
+        metodosOrdenamiento.printArreglo(palabrasOrdenadas);
     }
 }
